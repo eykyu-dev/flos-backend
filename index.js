20cbb5d4fba6b7f7e13f41156932bbd644755c20
Ethan Yu <eykyudev@gmail.com>
2023-05-30 16:56:19 -0700

    added models

--- index.js
+++ index.js
@@ -17,3 +17,4 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.listen(3000, () => console.log('Server is running on port' + PORT));
+
