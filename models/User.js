5b416e24ce5a49eed5108e61709c67b451e84d53
Ethan Yu <eykyudev@gmail.com>
2023-06-04 15:54:34 -0700

    changing

--- models/User.js
+++ models/User.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
-const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -13,43 +10,11 @@ const userSchema = new mongoose.Schema({
     email: {
        type: String,
        required: true,
-       unique: true,
-       lowercase: true,
-         validate( value ) {
-               if( !validator.isEmail( value )) {
-                    throw new Error('Email is invalid')
-                     }
-                }
-      },
+       unique: true,  m 
+    },
     password: {
         type: String,
-        required: true,
-        minLength: 7,
-        trim: true,
-        validate(value) {
-           if( value.toLowerCase().includes('password')) {
-           throw new Error("password can't contain password")
-          }
-       }
-    },
-    tokens: [{
-      token: {
-      type: String,
-      required: true
-        }
-      }]
-    }, {
-    timestamps: true
+        required: true
+    }
 })
-
-const User = mongoose.model("User", userSchema);
-
-userSchema.methods.generateAuthToken = async function () {
-  const user = this
-   const token = jwt.sign({ _id: user._id.toString()},      process.env.JWT_SECRET)
-user.tokens = user.tokens.concat({token})
-  await user.save()
-  return token
-}
-
 module.exports = User;
